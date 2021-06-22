8aab7186cb767045608fe21556440c638f22313b
ankurk91 <pro.ankurk1@gmail.com>
2021-06-22 12:02:44 +0530

    Add v-cloak to bootstrapVue component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,19 @@
 # Changelog
 
 ## 2.3.0
+
 * Drop php 7.3 support
+* Add `v-cloak` to bootstrapVue component
 
 ## 2.1.0
+
 * Add bootstrap vue support
 
 ## 2.0.0
+
 * Requires Laravel v6.x+
 * Test against php v7.4
 
 ## 1.0.0
+
 * Initial release
--- resources/views/bootstrapVue.blade.php
+++ resources/views/bootstrapVue.blade.php
@@ -1,5 +1,5 @@
 @if (session()->has('alert.message'))
-  <b-alert variant="{{ session()->get('alert.style') }}" fade show dismissible>
+  <b-alert v-cloak variant="{{ session()->get('alert.style') }}" fade show dismissible>
     {!! session()->get('alert.message') !!}
   </b-alert>
 @endif
