81043576a2802fa3f380b0d4e6779afec8aa9698
ankurk91 <pro.ankurk1@gmail.com>
2022-03-26 13:44:34 +0530

    Add Vanilla js support

---
+++ resources/views/bootstrapVanilla.blade.php
@@ -0,0 +1,13 @@
+@if (session()->has('alert.message'))
+  <div class="alert alert-{{ session()->get('alert.style') }} alert-dismissible fade in show" role="alert">
+    <button type="button"
+            class="close"
+            data-dismiss="alert"
+            aria-label="Close"
+            onclick="this.parentNode.remove()">
+      <span aria-hidden="true">&times;</span>
+    </button>
+
+    {!! session()->get('alert.message') !!}
+  </div>
+@endif
